d5dc37a417f3048a7e63b0e60446e30db62f765a
zhangqiuming <zhqm03@gmail.com>
2013-02-04 08:24:32 +0800

    recommand

--- tmp/recommandations.py
+++ tmp/recommandations.py
@@ -3,8 +3,7 @@
 # vim set expandtab
 from math import sqrt
 
-def loadMovieLens():
-    path="./movielens"
+def loadMovieLens(path="./data/movielens"):
     movies = {}
     for line in open(path + "/u.item"):
         id, title = line.split("|")[0:2]
@@ -18,44 +17,96 @@ def loadMovieLens():
     return prefs
 
 
-def simDistance(prefs, u1, u2):
-    si = {}
-    for item in prefs[u1]:
-        if item in prefs[u2]:
-            si[item] = 1
+def sim_distance(dict1, dict2):
+    si = [item for item in dict1 if item in dict2]
 
     if len(si) == 0: return 0
-    sum_of_squares = sum(pow(prefs[u1][item] - prefs[u2][item], 2) for item in si)
+    sum_of_squares = sum(pow(dict1[item] - dict2[item], 2) for item in si)
     return 1 / (1+sqrt(sum_of_squares))
 
 
-def topMatches(prefs, person, n=5, similarity=simDistance):
+def topMatches(prefs, person, n=5, similarity=sim_distance):
     scores = []
     for other in prefs :
         if other!=person:
-            scores.append((similarity(prefs, person, other), other))
+            scores.append((similarity(prefs[person], prefs[other]), other))
 
     scores.sort()
     scores.reverse()
     return scores[0:n]
 
+def recommand(prefs, person, n=5, similarity = sim_pearson):
+    totals = {}
+    simSums = {}
+    for other in prefs:
+        if other is person:
+            continue
+        sim = similarity(prefs[person], prefs[other])
 
-def recommand(prefs, person, n):
-    #users = topMatches(prefs, person, n=10)
-    #for score, other in users:
-        #score *pres[other][item] for item in prefs[other]
-    pass
+        if sim < 0: continue
+
+        for item in prefs[other]:
+            if item not in prefs[person] or prefs[person][item] == 0:
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
+
+    print len(totals)
+    print len(simSums)
+
+    rankings = [(total/simSums[item], item) for item, total in totals.items() if simSums[item] > 0]
+
+    rankings.sort()
+    rankings.reverse()
+
+    return rankings[0:n]
+
+
+def transformPrefs(prefs):
+    result = {}
+    for person in prefs:
+        for item in prefs[person]:
+            result.setdefault(item, {})
+            result[item][person] = prefs[person][item] 
+    return result
+
+def sim_pearson(dict1, dict2):
+
+
+    si = [item for item in dict1 if item in dict2]
+    n = len(si)
+    if not n:
+        return 0
+
+    sumXY = sum([dict1[item] * dict2[item] for item in si])
+    sumX  = sum([dict1[item] for item in si])
+    sumY  = sum([dict2[item] for item in si])
+    sumX2 = sum([pow(dict1[item], 2) for item in si])
+    sumY2 = sum([pow(dict2[item], 2) for item in si])
+
+
+    numerator = n *sumXY - sumX * sumY
+    denominator = sqrt(n * sumX2 - pow(sumX, 2)) * sqrt(n*sumY2 - pow(sumY, 2))
+    if denominator == 0:
+        return 0
+
+    r = numerator / denominator
+    return r
 
 def relevancy(sort1, sort2):
     dict1 = {}
     for k,v in enumerate(sort1):
         dict1[v] = k
     dict2 = {}
-    for i in enumerate(sort2):
+    for k,v in enumerate(sort2):
         dict2[v] = k
-    total = sum(pow(dict1[s] - dict2[s], 2) for s in dict1 if s in dict2)
-    return sqrt(total) /sqrt(sum(pow(s,2) for s in dict1) * sum(pow(s,2) for s in dict2))
+    return sim_pearson(dict1, dict2) / 2 + 0.5
 
 
-
+def sim_jaccob(dict1, dict2):
+    si = [item for item in dict1 if item in dict2]
+    factor = len(si)*1.0 / (len(prefs[u1])+ len(prefs[u2]) - len(si))
+    return factor
 
