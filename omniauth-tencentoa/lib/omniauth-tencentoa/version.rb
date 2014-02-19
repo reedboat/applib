4363be249231c3153d27a0276307ccd4bdcbbb65
zhangqiuming <zhqm03@gmail.com>
2014-02-19 17:20:05 +0800

    fix omniauth

--- omniauth-tencentoa/lib/omniauth-tencentoa/version.rb
+++ omniauth-tencentoa/lib/omniauth-tencentoa/version.rb
@@ -1,5 +1,5 @@
 module OmniAuth
   module TencentOA
-    VERSION = "0.1.0"
+    VERSION = "0.2.7"
   end
 end
--- omniauth-tencentoa/lib/omniauth/strategies/tencentoa.rb
+++ omniauth-tencentoa/lib/omniauth/strategies/tencentoa.rb
@@ -5,32 +5,35 @@ module OmniAuth
     module Strategies
         class TencentOA
             include OmniAuth::Strategy
-            option :fields, [:name, :password]
-            option :uid_field, :name
+            option :fields, [:login_name, :password]
+            option :uid_field, :login_name
 
             attr_accessor :auth_info
 
 
             def request_phase
                 #redirect client.auth_code.authorize_url({:redirect_uri => callback_url}.merge(options.authorize_params))
-                form = OmniAuth::Form.new(:title => "User Info", :url => callback_path)
-                options.fields.each do |field|
-                    form.text_field field.to_s.capitalize.gsub("_", " "), field.to_s
-                end
+                form = OmniAuth::Form.new(:title => "Tencent Outlook Account", :url => callback_path)
+                form.text_field "Rtx name", 'login_name'
+                form.password_field "Outlook password", 'password'
                 form.button "Sign In"
                 form.to_response
             end
 
             uid do
-                request.params['name']
+                request.params['login_name']
             end
 
             info do
-                { :name => @auth_info[:name], :nickname => @auth_info[:chinese_name],   :password => request.params['password'] }
+                { 
+                    :name  => @auth_info[:chinese_name].to_s,
+                    :email => @auth_info[:login_name] + '@tencent.com',
+                    :uid   => @auth_info[:login_name].to_s
+                }
             end
 
             credentials do
-                {:token => @auth_info[:token]}
+                {:token => @auth_info[:token].to_s, :secret => @auth_info[:key].to_s}
             end
 
             extra do 
@@ -43,10 +46,17 @@ module OmniAuth
 
             def callback_phase
                 url = 'http://10.6.12.14/services/passportservice.asmx?WSDL'
+                username = request['login_name'].to_s
+                password = request['password'].to_s
+
                 client = Savon.client(wsdl:url)
-                response = client.call(:AuthenticateViaLdap, message:{loginName => username, password => password})
-                self.auth_info = response.body
-                log :info, self.auth_info.to_yaml
+                begin
+                    response = client.call(:authenticate_via_ldap, message:{:loginName => username, :password => password})
+                rescue Exception => e
+                    fail!(:auth_failed, e) 
+                end 
+                @auth_info = response.body[:authenticate_via_ldap_response][:authenticate_via_ldap_result]
+                log :info, @auth_info.to_s
                 super
             end
         end
